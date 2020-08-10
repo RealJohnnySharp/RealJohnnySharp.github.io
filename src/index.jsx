8e6e52cb16c69b1d5c429f874fa6526c99349447
Johnny Sharp <johnny@johnnycsharp.com>
2020-08-09 22:48:27 -0400

    Attempt webp image with jpg fallback

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,7 @@ import './Fonts/Roboto-Regular.ttf';
 import './Fonts/Roboto-Bold.ttf';
 import './Fonts/Roboto-BoldItalic.ttf';
 import './index.scss';
+import './utils/modernizr-custom';
 import App from './Components/App';
 import * as serviceWorker from './serviceWorker';
 
